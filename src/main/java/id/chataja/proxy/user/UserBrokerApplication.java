3983260b5dfd1fd2a08ed13ac42245cad3119b21
Ahmad R. Djarkasih <djarkasih@gmail.com>
2020-01-29 12:47:59 +0700

    Spring Boot rest skeleton added.

---
+++ src/main/java/id/chataja/proxy/user/UserBrokerApplication.java
@@ -0,0 +1,13 @@
+package id.chataja.proxy.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBrokerApplication.class, args);
+	}
+
+}
